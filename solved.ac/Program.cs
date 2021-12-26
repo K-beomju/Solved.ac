9a2c1a203db47fa25d047402a2cc77f544686a2b
Kimbeomju <64351062+K-beomju@users.noreply.github.com>
2021-12-26 15:38:00 +0900

    초기 커밋.

---
+++ solved.ac/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace solved.ac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
