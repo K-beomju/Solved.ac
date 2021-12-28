19f670279688e3e96b02f79d92b05742065e7437
Kimbeomju <64351062+K-beomju@users.noreply.github.com>
2021-12-28 14:46:38 +0900

    BronzeV 등급 문제

--- solved.ac/Program.cs
+++ solved.ac/Program.cs
@@ -5,8 +5,10 @@ namespace solved.ac
     class Program
     {
         static void Main(string[] args)
-        {   
-            Console.WriteLine("Hello World!");
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a + b);
         }
     }
 }
